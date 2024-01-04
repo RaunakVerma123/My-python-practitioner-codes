6f30ba457774d2f2d61c68c0c0bb36c6e76e3b20
Raunak Verma <mrraunakverma@gmail.com>
2024-01-04 12:24:51 +0530

    Higher-Lower-game

---
+++ High_Low_game/main.py
@@ -0,0 +1,57 @@
+import random 
+import os
+from art import logo
+from art import vs
+from game_data import data
+
+SCORE=0
+
+def clear_console():
+  os.system('cls' if os.name=='nt' else 'clear')
+
+def assign_dictionary(data):
+  '''Returns a random dictionary from the list.'''
+  return random.choice(data)
+
+def game_structure(a,b):
+  '''Prints the basic structure of the game.'''
+  print(f"Compare A: {a['name']}, {a['description']}, {a['country']}")
+  print(vs)
+  print(f"Against B: {b['name']}, {b['description']}, {b['country']}")
+
+def check_answer(a,b):
+  '''Checks the greater value and returns 'A' and 'B' accordingly.'''
+  if a>b:
+    return 'A'
+  else:
+    return 'B'
+
+def high_low(A,B,score):
+  end_of_game=False
+  while not end_of_game:
+     game_structure(A,B)
+     answer=check_answer(A['follower_count'],B['follower_count'])
+     guess=input("Who has more followers? Type 'A' OR 'B': ")
+     clear_console()
+     print(logo)
+
+     if answer==guess:
+      score+=1
+      print(f"You got it right! Current score: {score}")
+      A=B
+      B=assign_dictionary(data)
+      while B==A:
+        B=assign_dictionary(data)
+      high_low(A,B,score)
+
+     else:
+        print(f"You lose! Final score: {score}")
+        exit()
+    
+print(logo)
+A=assign_dictionary(data)
+B=assign_dictionary(data)
+while B==A:
+  B=assign_dictionary(data)
+
+high_low(A,B,SCORE)
